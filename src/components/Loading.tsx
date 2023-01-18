371eacf4fe02850fd1296b5bbd3b0ed6d08e1391
Adriel <adriel.fsantos@outlook.com>
2023-01-17 21:02:52 -0300

    Inicia projeto mobile

---
+++ src/components/Loading.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ActivityIndicator, View } from 'react-native';
+
+const Loading = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#09090a',
+      }}
+    >
+      <ActivityIndicator color='#7c3aed' />
+    </View>
+  );
+};
+
+export default Loading;
